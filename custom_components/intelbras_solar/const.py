c953b731182defb074f8e5c0972893e7822899f0
Brad Downey <brad@magic7s.com>
2024-05-01 10:41:31 -0700

    Update const.py
    Bump version

--- custom_components/intelbras_solar/const.py
+++ custom_components/intelbras_solar/const.py
@@ -3,7 +3,7 @@
 NAME = "Intelbras Solar"
 DOMAIN = "intelbras_solar"
 DOMAIN_DATA = f"{DOMAIN}_data"
-VERSION = "0.0.4"
+VERSION = "0.0.5"
 ATTRIBUTION = ""
 ISSUE_URL = "https://github.com/magic7s/intelbras-solar/issues"
 
